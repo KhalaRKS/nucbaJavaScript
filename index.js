b7ac626c57cd4bc8b891609a03d0e3b70d7054c4
albanokainer <albanokainer@hotmail.com>
2022-09-06 21:38:50 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+
+
+// Variables con tipos de dato
+
+// toma de valor undenifed porque no esta definido aun
+var nombre 
+
+// toma de valor string porque lo definimos
+var apellido = 'Kainer'
+
+// toma de valor string porque el '3' es un caracter
+var numero = "3";
+
+// toma de valor number porque el 33 es un numero
+var edad = 33
+var promedio = 33.33
+
+// toma de valor null porque se define como nulo
+var nulo = null
+
+// toma de valor undenif porque se define como indefinido
+var indefinido = undefined
+
+// toma de valor true porque se define como true
+var booleano = true
+
+// muestra el contenido de la variable apellido
+
+/* la funcion prompt() dispara una alerta para que el usuario ingrese
+ un valor por teclado */ 
+
+nombre = prompt('Ingrese su nombre')
+console.log(nombre + ' ' +apellido)
